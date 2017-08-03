4e25c7e9789ee7c6863fa0155a2c5fcce19e5751
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-08-03 01:21:52 -0700

    Add IBranch type for abstracting FileSystem

---
+++ Coon/Coon/Branch/BranchOf.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Coon.Branch
+{
+    public class BranchOf : IBranch
+    {
+        private readonly string _filePath;
+
+        public BranchOf(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public IInput AsInput()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IBranch GetParent()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<IBranch> GetChildren()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Coon/Coon/IBranch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Coon
+{
+    public interface IBranch
+    {
+        IInput AsInput();
+        
+        IBranch GetParent();
+
+        IList<IBranch> GetChildren();
+    }
+}
