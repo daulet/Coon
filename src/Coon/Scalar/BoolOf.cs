2f75f40c24e12b1ee266d960a66c67de79434bf9
Daulet <daulet@zhanguzin.kz>
2017-10-24 01:49:54 -0700

    Add BoolOf

---
+++ src/Coon/Scalar/BoolOf.cs
@@ -0,0 +1,24 @@
+﻿using Coon.Text;
+
+namespace Coon.Scalar
+{
+    public sealed class BoolOf : IScalar<bool>
+    {
+        private readonly IText _text;
+
+        public BoolOf(string input)
+            : this(new TextOf(input))
+        { }
+
+        public BoolOf(IText text)
+        {
+            _text = text;
+        }
+        
+        public bool GetValue()
+        {
+            bool.TryParse(_text.AsString(), out bool result);
+            return result;
+        }
+    }
+}
---
+++ test/Coon.UnitTests/Scalar/BoolOfTests.cs
@@ -0,0 +1,26 @@
+﻿using Coon.Scalar;
+using Xunit;
+
+namespace Coon.UnitTests.Scalar
+{
+    public class BoolOfTests
+    {
+        [Fact]
+        public void GetValue_TrueString_True()
+        {
+            Assert.True(new BoolOf("True").GetValue());
+        }
+
+        [Fact]
+        public void GetValue_FalseString_False()
+        {
+            Assert.False(new BoolOf("false").GetValue());
+        }
+
+        [Fact]
+        public void GetValue_NonBooleanString_False()
+        {
+            Assert.False(new BoolOf("Not a boolean string").GetValue());
+        }
+    }
+}
